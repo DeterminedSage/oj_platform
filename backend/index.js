6e27c5f8e387fe810c4866e7461d56d92115c5a0
Akshat(DeterminedSage) <aksharma.nsut@gmail.com>
2025-05-20 16:58:16 +0530

    CRUD Basic complete

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,7 @@ app.use(cors());
 app.use('/auth', Router); 
 app.use('/contribute', Router); 
 app.use('/enquiry', Router);
+app.use('/report', Router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
