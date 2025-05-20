bf76302e24c247359608a41c3da24552f7f72491
Akshat(DeterminedSage) <aksharma.nsut@gmail.com>
2025-05-20 11:44:36 +0530

    upgrade to getQues error

--- backend/Routes/Router.js
+++ backend/Routes/Router.js
@@ -1,4 +1,4 @@
-const { signup, login, addQues } = require('../Controllers/Controller');
+const { signup, login, addQues, getQues } = require('../Controllers/Controller');
 const { signupValidation, loginValidation, addQuestionValidation } = require('../Middlewares/Validation');
 
 const router = require('express').Router();
@@ -8,5 +8,11 @@ router.post('/login', loginValidation, login);
 router.post('/signup', signupValidation, signup);
 
 router.post('/addQues',addQuestionValidation, addQues);
+
+router.get('/getQues', getQues);
+
+// router.get('/deleteQues', deleteQues);
+
+
 
 module.exports = router;
